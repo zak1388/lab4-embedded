cfbeea5ebb63982ad23a6a51b77571cf2ebdd063
Zakariyya Kadri <zakariyyakadri@gmail.com>
2024-10-24 17:47:50 +0100

    missed some

---
+++ inc/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void sequenceLED();
+void initSequenceLED_patternA();
+void sequenceLED_patternA();
+void initSequenceLED_patternB();
+void sequenceLED_patternB();
+
+#endif
